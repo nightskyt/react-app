dbb7635548dfcc90f8fbf15df2de319e0fdf6d6a
wangtian <wangt282@chinaunicom.cn>
2025-05-31 22:56:14 +0800

    Searching games

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,17 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assert/vite.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
-function NarvBar() {
+interface Props {
+  onSearch: (value: string) => void;
+}
+
+function NarvBar({ onSearch }: Props) {
   return (
     <HStack justifyContent={"space-between"}>
       <Image src={logo} boxSize="60px" />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
