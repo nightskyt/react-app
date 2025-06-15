7b75f57cc50b28d6bb7fff54339ba50f7d852d49
Tian Wang <wangtian96@hotmail.com>
2025-06-15 10:06:08 +0800

    Add game details page

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,6 +7,8 @@ export interface Game {
   id: number;
   slug: string;
   name: string;
+  description_raw: string;
+  released: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import NarvBar from "../components/NavBar";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <NarvBar />
       <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
