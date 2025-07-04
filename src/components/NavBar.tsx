e12b93ef44d81cec236706ce3f0374cf80d847e4
Tian Wang <wangtian96@hotmail.com>
2025-07-04 23:28:18 +0800

    Fix navbar search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assert/vite.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "@tanstack/react-router";
 
 function NarvBar() {
   return (
     <HStack justifyContent={"space-between"} paddingY={3}>
-      <Image src={logo} boxSize="60px" />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit={"cover"}/>
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,18 +1,21 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useNavigate } from "@tanstack/react-router";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../stores/gameQueryStore";
 
 const SearchInput = () => {
-  const setSearchText = useGameQueryStore((s) => s.setSearchText);
-
   const ref = useRef<HTMLInputElement>(null);
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         if (ref.current) {
           setSearchText(ref.current.value);
+          navigate({to: "/"});
         }
       }}
     >
